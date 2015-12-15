c7315851adef4eb81c7eb0e9ccdc95fd61942960
namitn1 <namitn1@users.noreply.github.com>
2015-12-16 03:26:36 +0530

    Create no.js

---
+++ no.js
@@ -0,0 +1,34 @@
+var list = ["selection", "link", "page", "image"];
+
+for(i=0;i<list.length;i++) {
+
+	var context = list[i];
+	var abc = "Share this " + context + " via twitter";
+	chrome.contextMenus.create({
+		title: abc,
+		contexts: [context],
+		onclick: follow,
+		id : context
+	});
+}
+function follow(data, tab) {
+		//alert(data.selectionText);
+		switch(data.menuItemId) {
+			case "selection" : 
+			chrome.windows.create({url: "https://twitter.com/intent/tweet?text=" + encodeURIComponent(data.selectionText), type: "panel" });
+			break;
+
+			case "link" : 
+			chrome.windows.create({url: "https://twitter.com/intent/tweet?url=" + encodeURIComponent(data.linkUrl), type: "panel" });
+			break;
+
+			case "page" : 
+			chrome.windows.create({url: "https://twitter.com/intent/tweet?text=" + encodeURIComponent(tab.title) + "&url=" + (data.pageUrl),type: "panel" });
+			break;
+
+			case "image" :
+			chrome.windows.create({url: "https://twitter.com/intent/tweet?url=" + encodeURIComponent(data.srcUrl), type: "panel" });
+			break;
+		}
+		
+}
